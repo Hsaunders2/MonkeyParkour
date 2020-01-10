fb5045beba940cc6e5e666e2e92e2fa09a11754e
Hsaunders2 <43548015+Hsaunders2@users.noreply.github.com>
2020-01-10 08:35:04 +0000

    Last quick update

---
+++ prototype/Parkour Project/Assets/Scripts/win3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class win3 : MonoBehaviour
+{
+
+
+    private Scene scene;
+
+    void Start()
+    {
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Application.Quit();
+    }
+}
